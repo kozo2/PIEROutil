185aae6a9ca1e2fb984de9c0b35bd44800b4319f
Kozo Nishida <knishida@riken.jp>
2015-02-21 14:27:37 +0900

    add inference option

--- R/PIEROutil.R
+++ R/PIEROutil.R
@@ -1,32 +1,37 @@
 #library(SPARQL)
 #library(hash)
 
+getInfo <- function(exfactor, inference = FALSE, limit = 0, endpoint="http://www.genome.jp/sparql/reactionontology/"){
 
-
-getInfo <- function(exfactor, limit = 0, endpoint="http://www.genome.jp/sparql/reactionontology/"){
-
-    limitC = ""
-    if (limit != 0)
-    if (is.numeric(limit) && ! is.null (grep ("/.",limit)))
-    limitC = paste( " limit " , limit)
-    else
-    warning ("limit should be an integer, limit omitted from the query")
-
-    query <- paste( "SELECT DISTINCT *\n",
-                    "WHERE { \n",
+  limitC = ""
+  if (limit != 0)
+  if (is.numeric(limit) && ! is.null (grep ("/.",limit)))
+  limitC = paste( " limit " , limit)
+  else
+  warning ("limit should be an integer, limit omitted from the query")
+  
+  sparql_base <- paste( "SELECT DISTINCT *\n",
+                        "WHERE { \n",
                         exfactor, " ?p ?o . \n",
-                    "}  \n",
-             limitC )
+                        "}  \n",
+                 limitC )
 
-    message("Performing query please wait...")
+  if(inference) {
+    query <- paste( "DEFINE input:inference 'http://reactionontology.org/inference' \n", sparql_base)
+  }
+  else {
+    query <- sparql_base
+  }
 
-    res <- tryCatch({
-                SPARQL(url=endpoint,query)
-            },
-            error = function(err){
-                message("an error occured when trying to query for ensembl genes ", err)
-            })#end tryCatch
-    
-    #res<-SPARQL(url=endpoint,query)
-    return (res$results)    
+  message("Performing query please wait...")
+
+  res <- tryCatch({
+              SPARQL(url=endpoint,query)
+          },
+          error = function(err){
+              message("an error occured when trying to query for ensembl genes ", err)
+          })#end tryCatch
+  
+  #res<-SPARQL(url=endpoint,query)
+  return (res$results)    
 }
